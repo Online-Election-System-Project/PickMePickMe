894329bb24ca712e6cd06ed625ca1f9785ff7ddb
wnsgud0428 <wnsgud0428@naver.com>
2021-12-05 15:55:03 +0900

    feat: 선거 등록 form에 place holder 추가

--- elections/forms.py
+++ elections/forms.py
@@ -15,6 +15,21 @@ class ElectionRegisterForm(forms.ModelForm):
             "election_period_end",
             "election_type",
         ]
+        widgets = {
+            "title": forms.TextInput(attrs={"placeholder": "대한민국 제20대 대통령 선거"}),
+            "register_period_begin": forms.DateTimeInput(
+                attrs={"placeholder": "2020-01-01 09:00:00"}
+            ),
+            "register_period_end": forms.DateTimeInput(
+                attrs={"placeholder": "2020-01-02 09:00:00"}
+            ),
+            "election_period_begin": forms.DateTimeInput(
+                attrs={"placeholder": "2020-01-11 09:00:00"}
+            ),
+            "election_period_end": forms.DateTimeInput(
+                attrs={"placeholder": "2020-01-12 09:00:00"}
+            ),
+        }
 
     def save(self, *args, **kargs):
         election = super().save(commit=False)
