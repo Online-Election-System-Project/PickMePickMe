f1f78209b328f466f169ad8b6f5b2cd28dcd1ede
wnsgud0428 <wnsgud0428@naver.com>
2021-12-11 18:21:29 +0900

    feat: 선거 List, DetailView 추가 / 선거 edit, delete 만듦

--- elections/urls.py
+++ elections/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name = "elections"
 
 urlpatterns = [
+    path("", views.ElectionListView.as_view(), name="elections"),
+    path("<int:pk>/", views.ElectionDetailView.as_view(), name="detail"),
     path(
         "register/",
         views.ElectionRegisterView.as_view(),
         name="election-register",
     ),
+    path("<int:pk>/edit/", views.EditElectionView.as_view(), name="edit-election"),
+    path("<int:pk>/delete/", views.deleteElection, name="delete-election"),
 ]
--- elections/views.py
+++ elections/views.py
@@ -1,17 +1,64 @@
-from django.shortcuts import render
-from django.views.generic import FormView, ListView
-from django.urls import reverse_lazy
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, FormView, UpdateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic.detail import DetailView
 
-from . import forms
+from . import forms, models
 
-# Create your views here.
+
+class ElectionListView(ListView):
+
+    """ElectionListView Definition"""
+
+    model = models.Election
+    paginate_by = 12
+    paginate_orphans = 5
+    context_object_name = "elections"
+
+
+class ElectionDetailView(DetailView):
+
+    """ElectionDetailView Definition"""
+
+    model = models.Election
+    template_name = "elections/election_detail.html"
 
 
 class ElectionRegisterView(FormView):
     template_name = "elections/register.html"
     form_class = forms.ElectionRegisterForm
-    success_url = reverse_lazy("elections:election-register")
+    success_url = reverse_lazy("elections:election-register")  # todo: 선거 리스트로 보내기
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class EditElectionView(UpdateView):
+
+    model = models.Election
+    template_name = "elections/edit-election.html"
+    fields = (
+        "title",
+        "register_period_begin",
+        "register_period_end",
+        "election_period_begin",
+        "election_period_end",
+        "election_type",
+    )
+    success_url = reverse_lazy("elections:election-register")
+
+    def get_object(self, queryset=None):
+        # 지금 로그인 한 유저가 superuser가 아니면 404오류 띄우기
+        election = super().get_object(queryset=queryset)
+        if self.request.user.is_superuser == False:
+            raise Http404()
+        return election
+
+
+def deleteElection(request, pk):
+    if request.method == "POST":
+        election = models.Election.objects.get(pk=pk)
+        election.delete()
+        return redirect("elections/register")
